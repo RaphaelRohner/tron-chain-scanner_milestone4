0d297e585f35e0d682f9a50f4afb64937142c97c
Raphael Rohner <raphaelrohner@web.de>
2020-11-22 22:27:30 +0000

    Connected database to Heroku.

--- tron_chain_scanner/settings.py
+++ tron_chain_scanner/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -112,7 +113,7 @@ WSGI_APPLICATION = 'tron_chain_scanner.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+   'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
