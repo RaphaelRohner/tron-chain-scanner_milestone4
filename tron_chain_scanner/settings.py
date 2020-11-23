8dae0c3d8397e9104e06d2777b1ddb3cb7cd66f4
Raphael Rohner <raphaelrohner@web.de>
2020-11-23 21:25:58 +0000

    Fixed bucket name.

--- tron_chain_scanner/settings.py
+++ tron_chain_scanner/settings.py
@@ -176,7 +176,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'tron-scanner-staticfiles-user'
+    AWS_STORAGE_BUCKET_NAME = 'tron-chain-scanner-milestone4'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
