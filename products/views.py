9081bc7e3b016d811dd5c7a622acafc353bb9147
Raphael Rohner <raphaelrohner@web.de>
2020-11-25 20:40:49 +0000

    Added sort function to products.html.

--- products/views.py
+++ products/views.py
@@ -33,7 +33,7 @@ def all_products(request):
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
+            categories = Category.objects.filter(name__in=categories)  # pylint: disable=maybe-no-member
 
         if 'q' in request.GET:
             query = request.GET['q']
