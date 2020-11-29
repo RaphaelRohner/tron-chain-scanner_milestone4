016b20fd74a2c12a8706f07156f65f8e1057d8fb
Raphael Rohner <raphaelrohner@web.de>
2020-11-29 21:56:25 +0000

    Fixed webhook secret key and checked functionality.

--- checkout/models.py
+++ checkout/models.py
@@ -78,7 +78,7 @@ class OrderLineItem(models.Model):
                                     null=True, blank=True)  # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
-                                         null=False, blank=False, #
+                                         null=False, blank=False,
                                          editable=False)
 
     def save(self, *args, **kwargs):
