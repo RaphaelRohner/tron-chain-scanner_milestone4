f710f656d1f8fe2943eca9b64be190e3004cca7b
Raphael Rohner <raphaelrohner@web.de>
2020-11-29 16:37:17 +0000

    Added view to checkout > views.py to check if the save information checkbox is checked.

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+)
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -9,9 +12,29 @@ from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
+import json
 
 
 # Create your views here.
+# VIEW TO CHECK IF SAVE INFO TICK IS SET
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
+
+
+# VIEW TO SEND ORDER INFORMATION TO STRIPE
 def checkout(request):
     """
     VIEW FOR THE CHECKOUT FORM
@@ -86,7 +109,7 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
     )
 
-    print(intent)  
+    print(intent)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
