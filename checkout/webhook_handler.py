664252f91d8457b965c5f2d12c28e6925881c5bd
Raphael Rohner <raphaelrohner@web.de>
2020-12-11 16:44:05 +0000

    Second try with adjusted webhook_handler to send html and plain text order confirmation email.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -21,10 +21,9 @@ class StripeWH_Handler:
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
         cust_email = order.email
-        order_number = order.order_number
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
-            {'order': order_number})
+            {'order': order})
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.html',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
