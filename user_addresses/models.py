fba1c6e164fa5ca0920aa9f081f215ac598699e5
Raphael Rohner <raphaelrohner@web.de>
2020-12-16 14:04:36 +0000

    Set some of the address fields for additional addresses as required.

--- user_addresses/models.py
+++ user_addresses/models.py
@@ -11,14 +11,14 @@ class UserAddresses(models.Model):
     additional delivery addresses.
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    additional_full_name = models.CharField(max_length=50, null=True, blank=True)  # noqa: DJ01, E501
-    additional_phone_number = models.CharField(max_length=20, null=True, blank=True)  # noqa: DJ01, E501
-    additional_street_address1 = models.CharField(max_length=80, null=True, blank=True)  # noqa: DJ01, E501
+    additional_full_name = models.CharField(max_length=50, null=False, blank=False)  # noqa: DJ01, E501
+    additional_phone_number = models.CharField(max_length=20, null=False, blank=False)  # noqa: DJ01, E501
+    additional_street_address1 = models.CharField(max_length=80, null=False, blank=False)  # noqa: DJ01, E501
     additional_street_address2 = models.CharField(max_length=80, null=True, blank=True)  # noqa: DJ01, E501
-    additional_town_or_city = models.CharField(max_length=40, null=True, blank=True)  # noqa: DJ01, E501
+    additional_town_or_city = models.CharField(max_length=40, null=False, blank=False)  # noqa: DJ01, E501
     additional_county = models.CharField(max_length=80, null=True, blank=True)  # noqa: DJ01, E501
     additional_postcode = models.CharField(max_length=20, null=True, blank=True)  # noqa: DJ01, E501
-    additional_country = CountryField(blank_label='Country', null=True, blank=True)  # noqa: DJ01, E501
+    additional_country = CountryField(blank_label='Country', null=False, blank=False)  # noqa: DJ01, E501
 
     def __str__(self):
         return self.additional_full_name  # pylint: disable=maybe-no-member
