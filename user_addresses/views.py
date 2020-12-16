98aef45a95520a6f8ba5122255f5d490a3eeecf3
Raphael Rohner <raphaelrohner@web.de>
2020-12-16 13:29:27 +0000

    Added functionality to add a new address.

--- user_addresses/views.py
+++ user_addresses/views.py
@@ -1,24 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render  # , get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from profiles.models import UserProfile  # import the user's profile
+# from profiles.models import UserProfile  # import the user's profile
 from .models import UserAddresses
 from .forms import UserAddressesForm
 
 
 @login_required
 def address(request):
-    """ Display the user's addresses. """
-    profile = get_object_or_404(UserProfile, user=request.user)
+    """ Display address form and the user's addresses. """
+    # profile = get_object_or_404(UserProfile, user=request.user)
     addresses = UserAddresses.objects.filter(user=request.user)  # noqa: E501, pylint: disable=maybe-no-member
 
     if request.method == 'POST':
-        form = UserAddressesForm(request.POST, instance=profile)
+        form = UserAddressesForm(request.POST,
+                                 instance=UserAddresses(user=request.user))
         if form.is_valid():
             form.save()
-            messages.success(request, 'Address updated successfully')
+            messages.success(request, 'Address added successfully')
         else:
-            messages.error(request, 'Update failed. Please ensure the form is valid.')  # noqa: 501
+            messages.error(request, 'Action failed. Please ensure the form is valid.')  # noqa: 501
     else:
         form = UserAddressesForm()
 
