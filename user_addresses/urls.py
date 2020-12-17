96d2a8b9444aad7e4acb9859940997b1dfcdaeaa
Raphael Rohner <raphaelrohner@web.de>
2020-12-17 13:13:16 +0000

    Added list of available addresses.

--- user_addresses/urls.py
+++ user_addresses/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.address, name='addresses')
+    path('', views.address, name='addresses'),
+    path('<item_id>/', views.address, name='addresses')
 ]
